ebd116f43370ea4c93d7755782580494f8638c37
Bjoern <b.kscheschinski@stud.uni-goettingen.de>
2021-03-10 17:27:00 +0100

    binned, send only changes, nothing tested

--- MQTTtest/controller2motor.py
+++ MQTTtest/controller2motor.py
@@ -26,38 +26,93 @@ def print_help():
     print("\tPress Q to quit")
 
 
+class Motor:
+    def __init__(self, sensitivity=1, speed0 = 50, n_bins=10, setup_name=None, device_ID=None, mqtt_client=None, motor_name=None):
+        self.dx = 0
+        self.mqtt = MQTTDevice(setup=setup_name, device=device_ID, mqtt_client=mqtt_client)
+        self.sensitivity = sensitivity
+        self.bins = np.linspace(0, 1.01, n_bins)
+        self.motor_name = motor_name
+        self.speed = speed0
+
+    def get_new_dx(self, defl):
+        return np.sign(defl) * self.speed * np.power(self.bins[np.digitize(np.abs(defl), self.bins)], self.sensitivity)
+
+    def send(self, current_dx):
+        new_dx = self.get_new_dx(current_dx)
+        if self.dx != new_dx:
+            self.mqtt.send(self.motor_name, new_dx)
+            self.dx = new_dx
+        else:
+            pass
+
 
 def main():
     import pygame
-    # set parameters
+    # ============== set sever parameters ==============
     setup_name          = "S001"
     device_ID           = "RAS01"
     device_MQTT_name    = "RASPI_" + str(randint(0, 100000))
     mqtt_broker_ip      = "localhost"
+    
     mqtt_client_name    = "raspi1" # not necessary
     mqtt_client_pass    = "1ipsar" # not necessary
+
     mqtt_port           = 1883
     mqtt_keepalive      = 60
     mqtt_uselogin       = False
 
+    # ============== set joystick parameters ==============
+    messages_per_sec = 50
 
-    messages_per_sec = 10
-    
+    # using dx -> speed * binned(dx)^sensitivity
     speed = 50
+    n_bins = 10
+    sensitivity = 1.4
+    speed_increase = 1.2
 
     # init class and auto-connect to server
-    uc2 = MQTTtest(setup_name=setup_name,device_ID=device_ID,device_MQTT_name=device_MQTT_name,mqtt_broker_ip=mqtt_broker_ip,mqtt_client_name=mqtt_client_name,mqtt_client_pass=mqtt_client_pass,mqtt_port=mqtt_port,mqtt_keepalive=mqtt_keepalive,mqtt_uselogin=mqtt_uselogin)
+    uc2 = MQTTtest( setup_name=setup_name,
+                    device_ID=device_ID,
+                    device_MQTT_name=device_MQTT_name,
+                    mqtt_broker_ip=mqtt_broker_ip,
+                    mqtt_client_name=mqtt_client_name,
+                    mqtt_client_pass=mqtt_client_pass,
+                    mqtt_port=mqtt_port,
+                    mqtt_keepalive=mqtt_keepalive,
+                    mqtt_uselogin=mqtt_uselogin)
 
     # add the motors
-    uc2.mqtt_register_devices(device_name='Motor_z',device_ID='OCM21')
-    uc2.mqtt_register_devices(device_name='Motor_x',device_ID='OCM21')
-    uc2.mqtt_register_devices(device_name='Motor_y',device_ID='OCM21')
- 
+    motor_x = Motor(    sensitivity=sensitivity, 
+                        speed0=speed, 
+                        n_bins=n_bins, 
+                        setup_name=setup_name, 
+                        device_ID="OCM21", 
+                        motor_name="MM_X", 
+                        mqtt_client = uc2.mqtt_client)
+
+    motor_y = Motor(    sensitivity=sensitivity, 
+                        speed0=speed, 
+                        n_bins=n_bins, 
+                        setup_name=setup_name, 
+                        device_ID="OCM21", 
+                        motor_name="MM_Y",
+                        mqtt_client = uc2.mqtt_client)
+
+
+    motor_z = Motor(    sensitivity=sensitivity, 
+                        speed0=speed, 
+                        n_bins=n_bins, 
+                        setup_name=setup_name, 
+                        device_ID="OCM21", 
+                        motor_name="MM_Z",
+                        mqtt_client = uc2.mqtt_client)
+                     
+
+    
+    # ============== initialize the joystick ==============
     pygame.init()
-
-    front_screen = pygame.font.SysFont('Comic Sans MS', 30)
-
-
+    # front_screen = pygame.font.SysFont('Comic Sans MS', 30)
     clock = pygame.time.Clock() # Used to manage how fast the screen updates.
 
     joystick_count = pygame.joystick.get_count()
@@ -69,12 +124,11 @@ def main():
         print("Number of joysticks: {}".format(joystick_count))
         joystick = pygame.joystick.Joystick(0)
         joystick.init()
-    done =False
     
+    done = False
     print_help()
 
     if not use_controller:
-        screen = pygame.display.set_mode((400,300))
         while not done:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
@@ -82,38 +136,59 @@ def main():
                         done = True
                     if event.key == pygame.K_a or event.key == pygame.K_d:
                         if event.key == pygame.K_a:
-                            speed -= 1
+                            motor_x.speed /= speed_increase
+                            motor_y.speed /= speed_increase
+                            motor_z.speed /= speed_increase
                         if event.key == pygame.K_d:
-                            speed += 1
+                            motor_x.speed *= speed_increase
+                            motor_y.speed *= speed_increase
+                            motor_z.speed *= speed_increase
 
             keys = pygame.key.get_pressed()
             if keys[pygame.K_LEFT]:
-                uc2.devices['Motor_x'].send("MM_X",-speed)
+                motor_x.send(-1)
             elif keys[pygame.K_RIGHT]:
-                uc2.devices['Motor_x'].send("MM_X",speed)
-            else: 
-                uc2.devices['Motor_x'].send("MM_X",0)
+                motor_x.send(1)
+            else:
+                motor_x.send(0)
+            
+            if keys[pygame.K_DOWN]:
+                motor_y.send(-1)
+            elif keys[pygame.K_UP]:
+                motor_y.send(1)
+            else:
+                motor_y.send(0)
+            
+            if keys[pygame.K_s]:
+                motor_z.send(-1)
+            elif keys[pygame.K_w]:
+                motor_z.send(1)
+            else:
+                motor_z.send(0)
 
-            textsurface = front_screen.render('RPMs {:.2f}:'.format(speed), False, (255, 255, 255))
-            screen.blit(textsurface,(0,0))
             clock.tick(messages_per_sec)            
                         
     else:                    
         while not done:
             for event in pygame.event.get():                
                 if joystick.get_button(Buttons.select):
-                        done = True
+                    done = True
+                if joystick.get_button(Buttons.up):
+                    motor_x.speed *= speed_increase
+                    motor_y.speed *= speed_increase
+                    motor_z.speed *= speed_increase
+                if joystick.get_button(Buttons.down):
+                    motor_x.speed /= speed_increase
+                    motor_y.speed /= speed_increase
+                    motor_z.speed /= speed_increase
+
             l3_axis = [joystick.get_axis(i) for i in [Axis.l3_x,Axis.l3_y]] # [0,1]
             r3_axis = [joystick.get_axis(i) for i in [Axis.r3_x,Axis.r3_y]]
             
-            if np.abs(l3_axis[0])>0.1:
-                uc2.devices['Motor_x'].send("MM_X", l3_axis[0]*speed)
-            else:
-                uc2.devices['Motor_x'].send("MM_X", 0)                
-            if np.abs(l3_axis[1])>0.1:
-                uc2.devices['Motor_y'].send(l3_axis[1])                
-            if np.abs(r3_axis[1])>0.1:
-                uc2.devices['Motor_z'].send(r3_axis[1])
+            motor_x.send(l3_axis[0])
+            motor_y.send(l3_axis[1])
+            motor_z.send(r3_axis[1])
+
             clock.tick(messages_per_sec)
             
     pygame.quit()
